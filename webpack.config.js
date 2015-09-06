aee76636b8a52fe26fef680df9c2f37040c265e9
Yushin Tatsumi <yushin.tatsumi@gmail.com>
2015-09-06 11:53:56 +0900

    Rename bundle files

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/scripts/app.js'
+    bundle: './src/scripts/app.js'
   },
   output: {
     path: 'build',
